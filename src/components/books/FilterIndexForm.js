d7f3dbb34716cbd6dc24298ce1a3ee1f059d2845
cjones1047 <cjmj1047@gmail.com>
2022-08-04 13:44:38 -0500

    Data from  3rd-party API is successfully returned with search form submission

--- src/components/books/FilterIndexForm.js
+++ src/components/books/FilterIndexForm.js
@@ -2,24 +2,47 @@ import './FilterIndexForm.css'
 
 import { Form, Button } from 'react-bootstrap'
 import { useState } from 'react'
+import axios from 'axios'
 
 const FilterIndexForm = (props) => {
 
     const [searchValue, setSearchValue] = useState('')
-    console.log('current input value:', searchValue)
+    console.log('\ncurrent input value:', searchValue)
 
     const handleChange = (e) => {
-        setSearchValue(prevSearchValue => {
+        setSearchValue(() => {
             let updatedSearchValue = e.target.value
 
             return (updatedSearchValue)
         })
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(`\nsubmitted value:\n${searchValue}`)
+
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}&langRestrict:en&orderBy:relevance`)
+            .then((res) => {
+                const data = res.data.items
+                console.log(data)
+                // let book = {
+
+                // }
+                
+            })
+            .catch(err => console.log(err))
+    }
+
 	return (
-		<Form className="d-flex" style={{maxWidth: '550px', width: '100%', padding: '10px'}}>
+		<Form
+            onSubmit={handleSubmit}
+            className="d-flex" 
+            style={{maxWidth: '550px', width: '100%', padding: '10px'}}
+            >
             <Form.Control
                 id='search-book-field'
+                autoComplete='off'
                 type="search"
                 placeholder="Any book title here..."
                 className="me-2"
@@ -27,7 +50,7 @@ const FilterIndexForm = (props) => {
                 value={searchValue}
                 onChange={handleChange}
             />
-            <Button style={{whiteSpace: 'nowrap'}} variant="outline-secondary">
+            <Button type='submit' style={{whiteSpace: 'nowrap'}} variant="outline-secondary">
                 Search the web
             </Button>
         </Form>
