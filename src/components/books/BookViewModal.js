b04e84cf49875180d392d35f74f891908511737e
cjones1047 <cjmj1047@gmail.com>
2022-08-06 13:01:11 -0500

    View book modal shows up correctly in mobile and wide viewports

--- src/components/books/BookViewModal.js
+++ src/components/books/BookViewModal.js
@@ -6,7 +6,7 @@ const BookViewModal = (props) => {
     } = props
     
     console.log(window.innerWidth)
-    
+
     if(window.innerWidth > 500)
     return (
         <Container fluid style={{maxWidth:'700px'}}>
@@ -26,7 +26,26 @@ const BookViewModal = (props) => {
                 {bookInViewModal.description}
             </Row>
         </Container>
-    );
+    ) 
+    else return (
+        <Container fluid style={{maxWidth:'700px'}}>
+            <Row className='justify-content-center'>
+                <div style={{display:'flex', justifyContent: 'center', margin:'20px'}}>
+                    <Card bg={'secondary'} text={'light'} style={{ width: 'fit-content', height: 'fit-content', margin: '20px', justifyContent: 'center' }}>
+                        <Card.Img variant="top" src={bookInViewModal.image} style={{ height: '300px', width: '190px' }} />
+                    </Card>
+                </div>
+            </Row>
+            <Row className='justify-content-center'>
+                <h2 style={{margin:'5px', textAlign:'center'}}>{bookInViewModal.title}</h2>
+                <h5 style={{margin:'5px', textAlign:'center',  fontStyle:'italic'}}>by {bookInViewModal.authors}</h5>
+                <h5 style={{margin:'5px', textAlign:'center', marginBottom: '30px'}}>Publisher: {bookInViewModal.publisher}</h5>
+            </Row>
+            <Row className='justify-content-center'>
+                {bookInViewModal.description}
+            </Row>
+        </Container>
+    )
 }
 
 export default BookViewModal
